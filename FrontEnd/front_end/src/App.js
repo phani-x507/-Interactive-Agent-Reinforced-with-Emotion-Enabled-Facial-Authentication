06b3ce940092d0a3ea34a87992c59fc2b256f087
phani-x507 <128954663+phani-x507@users.noreply.github.com>
2024-08-19 00:28:28 +0530

    Initialzation 1
    created and made frontend and backend folders

---
+++ FrontEnd/front_end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <button  className='btn btn-secondary' > This is bootstrap button</button>
+    </div>
+  )
+}
+
+export default App
